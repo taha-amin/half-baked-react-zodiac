c4ae29cc6b950b5213354f624a12e8b7f713371b
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-25 14:38:31 -0700

    finished writing ZodiacCard func in zodiaccard.js

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,12 @@
 import './ZodiacCard.css';
 
-export default function ZodiacCard() {
-  {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+export default function ZodiacCard({ name, dates }) {
+  /* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */
+  return (
+    <div className='zodiac-card'>
+      <img src={`../public/images/${name}.png`} />
+      <span className='name'>{name}</span>
+      <span>{dates}</span>
+    </div>
+  );
 }
