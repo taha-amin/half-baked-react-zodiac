394626e4e3a0f12659d8afcd5bbdbe2d9a4b8aee
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-25 14:24:08 -0700

    imported header and footer and called them inside App className

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+
 {/* import the zodiac array */}
+import Header from '../src/Header/Header.js';
+import Footer from '../src/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Header />
       {/* the Footer component needs a year passed in as the `year` prop */}
+      <Footer />
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
     </div>
   );
